7d00912a740142c8d3b48fed9a1cff25f6330143
Muslem Omar <mislimumer@gmail.com>
2022-11-09 18:20:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+// Task 1
+// Don't change the function name.
+function summer() {
+  
+}
+
+// Don't change the variable name.
+const aggr = summer();
+
+// Task 2
+// Don't change the variable name.
+let obj = {
+  
+};
+
+// Task 3 
+// Don't change the variable name.
+let obj2 = {
+  
+};
+
+// IMPORTANT: don't delete this, or the tests would fail!
+module.exports = {
+  aggr,
+  obj,
+  obj2
+};
